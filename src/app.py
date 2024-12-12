826c4efc1dbc4101e3b73acfcc7476e8447ab0a7
jllarose <jasminelarose6@gmail.com>
2024-12-12 02:05:24 +0000

    Completed KNN project

--- src/app.py
+++ src/app.py
@@ -1,41 +1,83 @@
-'''Simple command line utility to make movie recommendations
-based on KNN with cosine similarity.'''
+# Handle imports up-front
+import pandas as pd
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
 
-import pickle
+movies=pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/k-nearest-neighbors-project-tutorial/main/tmdb_5000_movies.csv")
+credits=pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/k-nearest-neighbors-project-tutorial/main/tmdb_5000_credits.csv")
 
-def get_movie_recommendations(movie_title, model, tfidf_matrix, encoded_data_df):
-    '''Takes a movie title string, looks up TFIDF feature vector for that movie
-    and returns title of top 5 most similar movies'''
+# Combine the datasets
+merged_ds = pd.merge(movies, credits, on = "title")
 
-    # Find the query movie in the encoded data, get the index
+merged_ds = merged_ds[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+
+# Make a copy to work with while encoding so that we have the original to go back to
+# if needed
+encoded_data_df=merged_ds.copy()
+
+# Empty list to hold extracted values
+extracted_values=[]
+
+# Loop on the elements of the cast column
+for json_string in merged_ds['cast']:
+
+    # Load the json string into a python dictionary
+    json_list=json.loads(json_string)
+
+    # Empty list to hold values from this element
+    values=[]
+
+    # Loop on the first three elements of the json list
+    for item in json_list[:3]:
+
+        # Extract the value for the name key
+        value=item['name']
+
+        # Add it to the list
+        values.append(value)
+
+    extracted_values.append(values)
+
+# Replace the 'cast' column with the extracted values
+encoded_data_df['cast']=extracted_values
+
+# Same for the 'keywords' column
+encoded_data_df['keywords']=merged_ds['keywords'].apply(lambda x: [item['name'] for item in json.loads(x)][:3] if pd.notna(x) else None)
+
+# And the 'genres' column
+encoded_data_df['genres']=merged_ds['genres'].apply(lambda x: [item['name'] for item in json.loads(x)][:3] if pd.notna(x) else None)
+
+
+# And the 'crew' column so that it is just the director
+encoded_data_df["crew"] = encoded_data_df["crew"].apply(lambda x: " ".join([crew_member['name'] for crew_member in json.loads(x) if crew_member['job'] == 'Director']))
+
+# Converting the Overview column to a list
+encoded_data_df['overview']=merged_ds['overview'].apply(lambda x: [x if pd.notna(x) else 'none'])
+
+# Creating the 'tags' column
+encoded_data_df["tags"]=encoded_data_df["overview"] + encoded_data_df["genres"] + encoded_data_df["keywords"] + encoded_data_df["cast"]
+
+# Converting 'tag' column to one long string instead of list
+encoded_data_df['tags'] = encoded_data_df['tags'].apply(lambda x: ', '.join(x))
+
+tags = encoded_data_df['tags']
+
+# K Nearest Neighbor Model Training:
+vectorize = TfidfVectorizer()
+tfidf_matrix=vectorize.fit_transform(tags)
+
+model = NearestNeighbors(n_neighbors = 5, algorithm = 'brute', metric = 'cosine')
+model.fit(tfidf_matrix)
+
+def get_movie_recommendations(movie_title):
     movie_index = encoded_data_df[encoded_data_df["title"] == movie_title].index[0]
-
-    # Get the distances and indexes of similar movies
     distances, indices = model.kneighbors(tfidf_matrix[movie_index])
-
-    # Extract the titles of the similar movie
     similar_movies = [(encoded_data_df["title"][i], distances[0][j]) for j, i in enumerate(indices[0])]
-    
     return similar_movies[1:]
 
-
-if __name__ == '__main__':
-
-    # Load the assets
-    model=pickle.load(open("../models/model.pkl", "rb"))
-    tfidf_matrix=pickle.load(open("../data/tfidf_matrix.pkl", "rb"))
-    encoded_data_df=pickle.load(open("../data/encoded_features_df.pkl", "rb"))
-
-    # Loop until user breaks
-    while True:
-
-        # Get a movie title from the user
-        input_movie=input("Enter a movie title: ")
-
-        # Get the recommendations
-        recommendations = get_movie_recommendations(input_movie, model, tfidf_matrix, encoded_data_df)
-
-        # Print the results
-        print("Film recommendations '{}'".format(input_movie))
-        for movie, distance in recommendations:
-            print("- Film: {}".format(movie))
+input_movie = "How to Train Your Dragon"
+recommendations = get_movie_recommendations(input_movie)
+print("Film recommendations '{}'".format(input_movie))
+for movie, distance in recommendations:
+    print("- Film: {}".format(movie))
